502d8dfcceda8bf052a78e5c669e98c6ab216973
Mana Pecar <mana.pecar@gmail.com>
2023-10-13 16:08:39 +0200

    Add Controller

---
+++ backend/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using productManagerApi.Models;
+
+namespace productManagerApi.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<Product> GetProducts()
+    {
+        var products = new List<Product>();
+
+        return products;
+    }
+}
+
--- backend/Program.cs
+++ backend/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<ApplicationDbContext>
 
 
 builder.Services.AddControllers();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
