97fe34cd40e7c7ae289aa69bc92fd8c0bc4030fc
Mana Pecar <mana.pecar@gmail.com>
2023-10-24 13:50:58 +0200

    Add ability to add category

--- backend/Controllers/CategoriesController.cs
+++ backend/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using productManagerApi.Data;
 using productManagerApi.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace productManagerApi.Controllers;
 
@@ -17,6 +18,7 @@ public class CategoriesController : ControllerBase
 
     [HttpGet]
     [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public IEnumerable<CategoryDTO> GetCategory([FromQuery] string? name)
     {
         IEnumerable<Category> categories = name is not null
@@ -28,6 +30,32 @@ public class CategoriesController : ControllerBase
         return categoryDtos;
     }
 
+    [HttpPost]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public ActionResult<CategoryDTO> CreateCategory(CreateCategoryRequest createCategoryRequest)
+    {
+        var category = new Category
+                      { Name = createCategoryRequest.Name };
+
+        if (category.Name is null or "")
+        {
+            return BadRequest();
+        }
+
+        context.Categories.Add(category);
+
+        context.SaveChanges();
+
+        var categoryDto = MapToCategoryDto(category);
+
+        return Created("", categoryDto);
+    }
+
+    
+
     private CategoryDTO MapToCategoryDto(Category category)
         => new()
         {
@@ -36,6 +64,12 @@ public class CategoriesController : ControllerBase
         };
 }
 
+public class CreateCategoryRequest
+{
+    [MaxLength(50)]
+    [Required]
+    public string Name { get; set; }
+}
 public class CategoryDTO
 {
     public int Id { get; set; }
